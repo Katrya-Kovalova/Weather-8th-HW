b30a514b0f6808c19a796f225f8bac45a62f3b24
Katrya-Kovalova <katya2441@gmail.com>
2022-12-24 22:12:34 +0200

    update all

--- src/app.js
+++ src/app.js
@@ -24,38 +24,30 @@ function formatDate(date) {
 }
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class=row`;
-  forecastHTML =
-    forecastHTML +
-    `
-            <div class="col-2">
-              <div class="weather-date">Thu</div>
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png"
-                alt=""
-                width="42"
-              />
-              <div class="weather-temperature">
-                <span class="weather-tem-max"> 12</span>
-                <span class="weather-tem-min"> 5</span>
-              </div>
-          </div>`;
-  forecastHTML =
-    forecastHTML +
-    `
-            <div class="col-2">
-              <div class="weather-date">Thu</div>
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png"
-                alt=""
-                width="42"
-              />
-              <div class="weather-temperature">
-                <span class="weather-tem-max"> 12</span>
-                <span class="weather-tem-min"> 5</span>
-              </div>
-          </div>`;
-  forecastHTML = `</div>`;
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 function displayWeatherCondition(response) {
